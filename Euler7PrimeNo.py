04a66c205f09fa10fdf73c568b40c081a73c6c1e
Rupinder <rupindermonga@gmail.com>
2020-01-09 20:05:54 +0530

    finding nth prime number

---
+++ Euler7PrimeNo.py
@@ -0,0 +1,20 @@
+#Getting nth prime number
+
+def PrimeNo(n):
+    i = 3
+    count = 1
+    prime = 2
+    while count < n:
+        for x in range(3, int(i** 0.5)+1, 2):
+            if i % x == 0:
+                break
+        else:
+            prime = i
+            count += 1
+        
+        i += 2
+    
+    return prime
+
+final = PrimeNo(10001)
+print(final)
